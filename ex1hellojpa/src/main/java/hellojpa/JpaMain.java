7cedc1cea4b8cbaf8ff90d22d0ea4056727df4f1
jintae_park <dktkd7044@gmail.com>
2021-10-23 00:15:10 +0900

    jpa 실습 영속성 컨텍스트

---
+++ ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,32 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        //EntityManagerFactory 애플리케이션 동작 시 하나만 존재해야함
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        //EntityManager 트랜잭션이 일어날때마다 만들어주는것
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
+
+            System.out.println("====================");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
