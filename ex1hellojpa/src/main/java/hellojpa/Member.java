d195e663725de86b254ba11a3f6196b3f8c2fc28
jintae_park <dktkd7044@gmail.com>
2021-11-30 23:04:46 +0900

    연관관계 매핑 실습

--- ex1hellojpa/src/main/java/hellojpa/Member.java
+++ ex1hellojpa/src/main/java/hellojpa/Member.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Member {
@@ -14,8 +11,36 @@ public class Member {
     private Long id;
 
     @Column(name = "USERNAME")
-    private String name;
+    private String username;
 
-    @Column(name = "TEAM_ID")
-    private Long teamID;
+   /* @Column(name = "TEAM_ID")
+    private Long teamID;*/
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
