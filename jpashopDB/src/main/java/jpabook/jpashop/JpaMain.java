0607209c777615ca34887409f5bdf1b7f19bdd99
jintae_park <dktkd7044@gmail.com>
2021-12-01 12:44:07 +0900

    다양한 연관관계 매핑 실습

--- jpashopDB/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashopDB/src/main/java/jpabook/jpashop/JpaMain.java
@@ -18,12 +18,7 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("ZZZZZZ");
 
-            em.detach(member);
-
-            System.out.println("====================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
