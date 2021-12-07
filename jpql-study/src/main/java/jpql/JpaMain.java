582fe567dd37ea19fcd0c6e6dd2ee12874ef10b2
jintae_park <dktkd7044@gmail.com>
2021-12-07 22:59:02 +0900

    jpql-study

---
+++ jpql-study/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,33 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setName("A");
+            member.setAge(10);
+            em.persist(member);
+
+            List<Member> resultList = em.createQuery("select m from Member m", Member.class).getResultList();
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+}
---
+++ jpql-study/src/main/java/jpql/Member.java
@@ -0,0 +1,48 @@
+package jpql;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id@GeneratedValue
+    private Long id;
+    private String name;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
