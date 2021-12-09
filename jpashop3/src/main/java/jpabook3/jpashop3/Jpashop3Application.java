0c13176c34bf2f78521d51ae98765138985d36cc
jintae_park <dktkd7044@gmail.com>
2021-12-10 01:44:07 +0900

    init jpashop3

---
+++ jpashop3/src/main/java/jpabook3/jpashop3/Jpashop3Application.java
@@ -0,0 +1,12 @@
+package jpabook3.jpashop3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpashop3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpashop3Application.class, args);
+	}
+}
---
+++ jpashop3/src/test/java/jpabook3/jpashop3/Jpashop3ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook3.jpashop3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpashop3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
