c64fd01b7327208c123aa5d7469bb8b537ec632f
jintae_park <dktkd7044@gmail.com>
2022-02-22 17:44:45 +0900

    재실습을 위해 모두 삭제

--- ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -13,33 +13,6 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        try {
-            /*em.flush(); //sql 버퍼 비우기
-            em.clear(); //영속성 컨텍스트 1차 캐시 초기화 */
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "1000000"));
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
 
-            member.getAddressHistory().add(new Address("old1", "street2", "20000"));
-            member.getAddressHistory().add(new Address("old2", "street3", "30000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            Member findMember = em.find(Member.class, member.getId());
-            
-
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-        emf.close();
     }
 }
